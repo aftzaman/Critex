d0d966deaad435e3175c65e2c8ec05b04f3c8d6f
Habib <aftzaman@gmail.com>
2019-07-24 08:15:26 +0400

    Designs Added

--- Critex/ViewController.swift
+++ Critex/ViewController.swift
@@ -18,3 +18,44 @@ class ViewController: UIViewController {
 
 }
 
+extension UITextField {
+    
+    func setLocationTextView()  {
+        
+        self.frame.size.height = 50
+        self.layer.cornerRadius = self.frame.height / 2
+        
+        //To apply Shadow
+        self.layer.shadowOpacity = 1
+        self.layer.shadowRadius = 3.0
+        self.layer.shadowOffset = CGSize.zero // Use any CGSize
+        self.layer.shadowColor = UIColor.gray.cgColor
+        
+        //To apply padding
+        let paddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = UITextField.ViewMode.always
+    }
+}
+
+
+@IBDesignable class Card: UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat = 13
+    @IBInspectable var shadowColor: UIColor? = UIColor.black
+    
+    @IBInspectable var shadowOffsetWidth: Int = 0
+    @IBInspectable var shadowOffsetHeight: Int = 1
+    
+    @IBInspectable var shadowOpacity: Float = 0.3
+    
+    override func layoutSubviews() {
+        layer.cornerRadius = cornerRadius
+        layer.shadowColor = shadowColor?.cgColor
+        layer.shadowOffset = CGSize(width: shadowOffsetWidth, height: shadowOffsetHeight);
+        let shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
+        layer.shadowPath = shadowPath.cgPath
+        layer.shadowOpacity = shadowOpacity
+    }
+}
+
